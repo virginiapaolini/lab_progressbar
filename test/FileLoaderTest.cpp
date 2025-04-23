60380cbfa1e68af47a9778d4486c03684e519813
virginiapaolini <virginia.paolini@edu.unifi.it>
2025-04-24 00:10:45 +0200

    gtesting versione finale

--- test/FileLoaderTest.cpp
+++ test/FileLoaderTest.cpp
@@ -3,6 +3,32 @@
 //
 #include <gtest/gtest.h>
 #include "../FileLoader.h"
+#include "Observer.h"
+
+class NotifiesCounter : public Observer {
+public:
+    explicit NotifiesCounter(FileLoader *file) : subject(file) {
+            attach();
+    }
+    ~NotifiesCounter() override {
+        detach();
+    }
+
+    void attach() override {
+        subject->subscribe(this);
+    }
+    void detach() override {
+        subject->unsubscribe(this);
+    }
+
+    void update(int progress) override {
+        numUpdates.push_back(progress);
+    }
+
+protected:
+    vector<int> numUpdates;
+    FileLoader* subject;
+};
 
 class FileLoaderTest : public ::testing::Test {
 protected:
@@ -10,7 +36,9 @@ protected:
         // tipo costruttore
         SourceFile* source1 = new SourceFile({"text.txt", "audio.mp3"});
         SourceFile* source2 = new SourceFile({"movie.mkv"});
-        FileLoaderTest* loader1 = new FileLoaderTest(source1);
+        SourceFile* source3 = new SourceFile({"app.apk"});
+        FileLoaderTest* loader1 = new FileLoaderTest;
+        NotifiesCounter* notifiesCounter = new NotifiesCounter(loader1);
     }
 
     void tearDown() {
@@ -18,7 +46,38 @@ protected:
         delete loader;
         delete source;
     }
+    SourceFile* source1;
+    SourceFile* source2;
+    SourceFile* source3;
+    FileLoaderTest* loader1;
+    NotifiesCounter* notifiesCounter;
 
-    SourceFile* source;
-    FileLoader* loader;
 };
+
+TEST_F(FileLoaderTest, CorrectResourceLoading){
+    loader1->addFilesToLoad(source1);
+    loader1->addFilesToLoad(source2);
+    loader1->addFilesToLoad(source3);
+    loader1->loadAll();
+
+ASSERT_EQ(notifiesCounter->numUpdates.size(), 4);
+
+// Controlla che la prog sia corretta (0, 25, 50, 100)
+vector<int> expectedProgress = {0, 25, 50, 100};
+EXPECT_EQ(notifiesCounter->numUpdates, expectedProgress);
+
+}
+
+TEST_F(FileLoaderTest, CorrectFilesLoading){
+loader1->addFilesToLoad(source1);
+loader1->addFilesToLoad(source2);
+loader1->addFilesToLoad(source3);
+ASSERT_EQ(loader1->filesToLoad.size(), 3);
+
+loader1->removeFilesToLoad(source1);
+loader1->removeFilesToLoad(source2);
+loader1->removeFilesToLoad(source3);
+
+ASSERT_TRUE(loader1->filesToLoad.empty());
+}
+
