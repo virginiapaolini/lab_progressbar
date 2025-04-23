5a6c3cb4d70cf6e65ffbc6ced588beccc576aaad
virginiapaolini <virginia.paolini@edu.unifi.it>
2025-04-23 15:54:46 +0200

    prima stesura, creazione obesrver e classi

---
+++ ProgressBar.h
@@ -0,0 +1,45 @@
+//
+// Created by Virginia Paolini on 23/04/25.
+//
+
+#ifndef LAB_PROGRESSBAR_PROGRESSBAR_H
+#define LAB_PROGRESSBAR_PROGRESSBAR_H
+#include "Observer.h"
+#include "FileLoader.h"
+
+class ProgressBar : public Observer {
+public:
+    explicit ProgressBar(FileLoader *file) : subject(file) {
+        attach();
+    }
+
+    ~ProgressBar() override {
+        detach();
+    }
+
+    void update(int progress) override {
+        cout << "Progress: [";
+        int progBar = progress / 10;
+        for (int it = 0; it < 10; it++) {
+            if (it < progBar)
+                cout << "#";
+            else
+                cout << "-";
+        }
+        cout << "] " << progress << "% \n";
+    }
+
+    void attach() override {
+        subject->subscribe(this);
+    }
+
+    void detach() override {
+        subject->unsubscribe(this);
+    }
+
+private:
+    FileLoader* subject;
+};
+
+
+#endif //LAB_PROGRESSBAR_PROGRESSBAR_H
