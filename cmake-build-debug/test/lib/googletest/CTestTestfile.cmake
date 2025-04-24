a86ebed36e6a5e864e8ade665e6227356872fbaa
virginiapaolini <virginia.paolini@edu.unifi.it>
2025-04-24 12:07:36 +0200

    improvements

---
+++ cmake-build-debug/test/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/virginiapaolini/CLionProjects/lab_progressbar/test/lib/googletest
+# Build directory: /Users/virginiapaolini/CLionProjects/lab_progressbar/cmake-build-debug/test/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
