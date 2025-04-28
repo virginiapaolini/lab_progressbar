2c805a6291fa9c4c7b9271e782fdd9e0899ccf5b
virginiapaolini <virginia.paolini@edu.unifi.it>
2025-04-28 18:45:21 +0200

    ultime modifiche post revisione

--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/virginiapaolini/CLionProjects/lab_progressbar/test
-# Build directory: /Users/virginiapaolini/CLionProjects/lab_progressbar/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/googletest")
